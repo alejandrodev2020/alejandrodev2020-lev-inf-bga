47e8519967be97f67dc9446c2e9ded23122c749b
alejandrodev2021 <favian.avila@full-assistance.net>
2023-06-05 12:44:05 -0400

    Lazy Load

--- README.md
+++
@@ -1,2 +0,0 @@
-# alejandrodev2020-lev-inf-bga
-Project React + Vite
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,33 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
+import React, { lazy,Suspense  } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
 import './index.css'
+const App = lazy(() => import('./App.tsx'));
+const Artists = lazy(() => import('./Artists.tsx'));
+const OtherComponent = React.lazy(() => import('./Artists'));
 
-render(<App />, document.getElementById('app') as HTMLElement)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  // <React.StrictMode>
+  //   <App />
+  //   <OtherComponent />
+  // </React.StrictMode>,
+
+
+//   <div >
+//   <Suspense fallback={<h1>Still Loading…</h1>}>
+//    <Artists />
+//    <App />
+//   </Suspense>
+//  </div>
+<BrowserRouter>
+<Routes>
+ 
+
+    <Route path="/" element={<App />} />
+    <Route path="blogs" element={<App />} />
+    <Route path="contact" element={<Artists />} />
+    <Route path="*" element={<OtherComponent />} />
+  
+</Routes>
+</BrowserRouter>
+)
